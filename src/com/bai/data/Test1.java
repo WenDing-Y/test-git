73267ad5960ff3e7f85623c5e9ea3aa71ae4a924
iareuniqe <1062698930@qq.com>
2018-04-03 22:19:05 +0800

    second push ,create Test1

---
+++ src/com/bai/data/Test1.java
@@ -0,0 +1,12 @@
+package com.bai.data;
+
+/**
+ * @author xxx_xx
+ * @date 2018/4/3
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("this is Test ");
+    }
+}
