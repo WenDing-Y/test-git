534d3e81b535a4fa4a502c54e9561bd7531fd6da
iareuniqe <1062698930@qq.com>
2018-04-03 22:13:15 +0800

    first push ,create project

---
+++ src/com/bai/data/Test.java
@@ -0,0 +1,12 @@
+package com.bai.data;
+
+/**
+ * @author xxx_xx
+ * @date 2018/4/3
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
